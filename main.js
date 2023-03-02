8f442356644c6c83147d785a8d1a0c88a008b4d8
Vico <vpontin93@gmail.com>
2023-03-02 12:33:41 -0300

    CORRIGIDO!
    Eu acho

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 */
 
 /* ---------------- DECLARAÇÕES ---------------- */
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { Configuration, OpenAIApi } = require("openai");
 
 /* ----------------- VARIÁVEIS ----------------- */
@@ -24,41 +24,30 @@ const configuration = new Configuration({
 	apiKey: config.openai.api_key,
 });
 const openai = new OpenAIApi(configuration);
+const last_messages = new Collection();
 
 /* ------------------ FUNÇÕES ------------------ */
-// Função para coletar a última mensagem enviada pelo BOT à certa pessoa
-function get_last_reply(guild, channel, username)
-{
-	// Get the guild and channel objects
-	// let guild = client.guilds.cache.get(guild_id);
-	// let channel = guild.channels.cache.get(channel_id);
-	// Get the ID of an user based on his nickname
-	let user_id = guild.members.cache.find(user_id => user_id.nickname === username);
-
-	// Fetch the last 10 messages in the channel
-	channel.messages.fetch({ limit: 10 }).then(messages => {
-		// Find the last message sent by the bot that mentions the specified user
-		let botMessage = messages.filter(m => m.author.bot && m.mentions.users.has(user_id)).first();
-
-		// Do something with the bot message
-		console.log(botMessage);
-		//console.log(`Last message sent by bot to user ${user_id}: ${botMessage.content}`);
-		// return botMessage.content;
-		return 0;
-	});
-}
-
 // Função para enviar uma pergunta ao GPTGPT e obter uma resposta
-async function openai_reply(guild, channel, username, message) {
-	let last_reply = get_last_reply(guild, channel, username);
+async function openai_reply(user_id, username, message) {
+	let last_message = "";
+	if (last_messages.has(user_id))
+	{
+		last_message = last_messages.get(user_id)
+	}
+	else
+	{
+		last_message = "Esta é a primeira conversa com " + username;
+	}
 	const completion = await openai.createChatCompletion({
 	  model: "gpt-3.5-turbo",
 	  messages: [
-		{role: "assistant", content: last_reply},
-		{role: "system", name: username, content: config.openai.context},
-		{role: "user", content: message}
+		{role: "assistant", content: last_message},
+		{role: "system", content: config.openai.context},
+		{role: "user", name: username, content: message}
 	],
 	});
+	last_messages.set(user_id, completion.data.choices[0].message.content);
+	// console.log(last_messages);
 	return completion.data.choices[0].message;
 }
 
@@ -79,7 +68,7 @@ client.on('messageCreate', async (msg) => {
 	{
 		msg.channel.sendTyping(); // Inicie a simulação de digitação
 		let response = "";
-		response = await openai_reply(msg.guild, msg.channel, msg.author.username, msg.cleanContent.replace(/@/g, ""));
+		response = await openai_reply(msg.author.id, msg.author.username, msg.cleanContent.replace(/@/g, ""));
 		msg.reply(response);
 	}
 });
