1b443d569c3602b31300f0f47c4aa690e0fada9e
Vico <vpontin93@gmail.com>
2023-03-02 11:42:38 -0300

    Tentando aprimorar o bot
    Ainda falta funcionar

--- main.js
+++ main.js
@@ -26,11 +26,38 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 /* ------------------ FUNÇÕES ------------------ */
-// Função para enviar uma pergunta ao GPT-3 e obter uma resposta
-async function openai_reply(message) {
+// Função para coletar a última mensagem enviada pelo BOT à certa pessoa
+function get_last_reply(guild, channel, username)
+{
+	// Get the guild and channel objects
+	// let guild = client.guilds.cache.get(guild_id);
+	// let channel = guild.channels.cache.get(channel_id);
+	// Get the ID of an user based on his nickname
+	let user_id = guild.members.cache.find(user_id => user_id.nickname === username);
+
+	// Fetch the last 10 messages in the channel
+	channel.messages.fetch({ limit: 10 }).then(messages => {
+		// Find the last message sent by the bot that mentions the specified user
+		let botMessage = messages.filter(m => m.author.bot && m.mentions.users.has(user_id)).first();
+
+		// Do something with the bot message
+		console.log(botMessage);
+		//console.log(`Last message sent by bot to user ${user_id}: ${botMessage.content}`);
+		// return botMessage.content;
+		return 0;
+	});
+}
+
+// Função para enviar uma pergunta ao GPTGPT e obter uma resposta
+async function openai_reply(guild, channel, username, message) {
+	let last_reply = get_last_reply(guild, channel, username);
 	const completion = await openai.createChatCompletion({
 	  model: "gpt-3.5-turbo",
-	  messages: [{role: "user", content: message}],
+	  messages: [
+		{role: "assistant", content: last_reply},
+		{role: "system", name: username, content: config.openai.context},
+		{role: "user", content: message}
+	],
 	});
 	return completion.data.choices[0].message;
 }
@@ -44,12 +71,15 @@ client.on('messageCreate', async (msg) => {
 	// Caso o autor seja um bot (ele mesmo incluído) não faz nada
 	if (msg.author.bot) return;
 	
+	// Caso a mensagem venha por DM (IMPLEMENTAR POSTERIORMENTE ALGUMA ROTINA SOBRE ISSO)
+	if (!msg.guild) return;
+	
 	// Responde a mensagem se ele for mencionado (e se for uma resposta com ping?)
 	if (msg.mentions.has(client.user.id))
 	{
 		msg.channel.sendTyping(); // Inicie a simulação de digitação
 		let response = "";
-		response = await openai_reply(msg.author.username + ": " + msg.cleanContent.replace(/@/g, ""));
+		response = await openai_reply(msg.guild, msg.channel, msg.author.username, msg.cleanContent.replace(/@/g, ""));
 		msg.reply(response);
 	}
 });
